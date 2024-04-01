3b9951fcb20e90a5da06c250dfb57cee679d8b3f
ricardo051199 <88699957+ricardo051199@users.noreply.github.com>
2024-04-01 02:25:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,27 +4,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-
-
-
-
 @app.route('/')
 def hola_mundo():
     return jsonify(mensaje="¡Hola desde el backend en Python!")
-
-@app.route('/modelo-black-scholes', methods=['POST'])
-def calcular_black_scholes():
-    try:
-        data = request.get_json()
-        edad = float(data.get['edad'])
-        estado_hp = float(data.get['estadoHp'])
-        ingresos = float(data.get['ingresos'])
-        deuda = float(data.get['deuda'])
-        cantidad_activos = float(data.get['cantidadActivos'])
-        
-        return jsonify(resultado='Resultado')
-    except ValueError:
-        return jsonify(error="Los datos proporcionados no son válidos"), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
