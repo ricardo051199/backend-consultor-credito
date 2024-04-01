4440831e3ab58f38c696844fc1228fe7d9ba7e7a
ricardo051199 <88699957+ricardo051199@users.noreply.github.com>
2024-04-01 03:15:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from pgmpy.models import BayesianModel
-from pgmpy.factors.discrete import TabularCPD
-from pgmpy.inference import VariableElimination
 
 app = Flask(__name__)
 CORS(app)
