af15fa02619164aaf3ed941e45fa0a5aa5ef8c1c
ricardo051199 <88699957+ricardo051199@users.noreply.github.com>
2024-04-01 03:07:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,5 @@ CORS(app)
 def hola_mundo():
     return jsonify(mensaje="¡Hola desde el backend en Python!")
 
-@app.route('/consultar-credito', methods=['GET'])
-def consultar_credito():
-    try:
-        return jsonify({'resultado': 'Resultado'})
-    except ValueError:
-        return jsonify(error="Los datos proporcionados no son válidos"), 400
-
 if __name__ == '__main__':
     app.run(debug=True)
